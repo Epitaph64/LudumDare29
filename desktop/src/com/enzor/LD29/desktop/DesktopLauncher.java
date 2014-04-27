4cdc58f1e859fe2a9a9963e18eff45aa3f5ea0af
Walter Macfarland <wjmacfarland@email.arizona.edu>
2014-04-26 19:22:25 -0700

    Added resource manager

--- desktop/src/com/enzor/LD29/desktop/DesktopLauncher.java
+++ desktop/src/com/enzor/LD29/desktop/DesktopLauncher.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.enzor.LD29.LudumDare29;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
+	public static void main(String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Ludum Dare 29";
 		config.useGL30 = false;
